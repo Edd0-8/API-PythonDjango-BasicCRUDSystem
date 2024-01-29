0b58e0af4d3a882d901ad28fddfa92fc9046ef21
eespinoz4 <eespinoza@netcore.cl>
2024-01-28 22:11:25 -0300

    Se agrego parametro de prueba a path hello

--- myapp/urls.py
+++ myapp/urls.py
@@ -3,6 +3,7 @@ from django.urls import path
 from myapp import views
 
 urlpatterns = [
-    path('', views.hello),
+    path('', views.index),
     path('about/', views.about),
+    path('hello/<str:username>', views.hello),
 ]
--- myapp/views.py
+++ myapp/views.py
@@ -2,8 +2,12 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 # Create your views here.
-def hello(request):
-    return HttpResponse("Hello World")
+def index(request):
+    return  HttpResponse("Index Page")
+
+def hello(request, username):
+    print(username)
+    return HttpResponse("<h2>Hello World</h2>")
 
 def about(request):
     return HttpResponse("About")
