5e2a2538231b7f3647ec9970d4e166a59d15d96b
eespinoz4 <eespinoza@netcore.cl>
2024-01-31 08:17:02 -0300

    Se agrego Task y Projects views

--- myapp/views.py
+++ myapp/views.py
@@ -1,13 +1,22 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, JsonResponse  
+from .models import Project, Task
+from django.shortcuts import get_object_or_404
 
 # Create your views here.
 def index(request):
     return  HttpResponse("Index Page")
 
 def hello(request, username):
-    print(username)
-    return HttpResponse("<h2>Hello World</h2>")
+    return HttpResponse("<h2>Hello %s</h2>" % username)
 
 def about(request):
-    return HttpResponse("About")
+    return HttpResponse('About')
+
+def projects(request):
+    projects = list(Project.objects.values())
+    return JsonResponse(projects, safe=False)
+
+def tasks(request, id):
+    task = get_object_or_404(Task, id=id)
+    return HttpResponse('task: %s' %task.title)
